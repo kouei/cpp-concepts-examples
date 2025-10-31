4b6dc6c259a63b5f40d3120ef3e9393d1b7d8550
kouei <kagigokoro@gmail.com>
2025-11-01 06:16:25 +0800

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <concepts>
+#include <cstddef>
+#include <functional>
+#include <set>
+#include <vector>
+
+// Without this HasPushBack, the compiler will produces error about redefinition
+// of add()
+template <typename CollT>
+concept HasPushBack =
+    requires(CollT c, CollT::value_type v) { c.push_back(v); };
+
+template <typename CollT, typename T>
+  requires HasPushBack<CollT>
+void add(CollT &coll, const T &val) {
+  coll.push_back(val);
+}
+
+template <typename CollT, typename T> void add(CollT &coll, const T &val) {
+  coll.insert(val);
+}
+
+int main() {
+  std::vector<int> coll1;
+  std::set<int> coll2;
+
+  static_assert(HasPushBack<std::vector<int>>);
+  static_assert(!HasPushBack<std::set<int>>);
+
+  add(coll1, 42);
+  add(coll2, 42);
+  return 0;
+}
