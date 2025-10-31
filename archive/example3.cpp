15c2363ead748f2ca73216b3bbedea8ba0792209
kouei <kagigokoro@gmail.com>
2025-11-01 06:43:53 +0800

    Add example3.cpp

---
+++ archive/example3.cpp
@@ -0,0 +1,34 @@
+#include <concepts>
+#include <cstddef>
+#include <functional>
+#include <set>
+#include <vector>
+
+template <typename CollT>
+concept HasPushBack =
+    requires(CollT c, CollT::value_type v) { c.push_back(v); };
+
+// The following does not work,
+// because decltype(coll) is std::vector<int>& rather than std::vector<int>
+//
+// void add(auto &coll, const auto &val)
+//   requires HasPushBack<decltype(coll)>
+// {
+//   coll.push_back(val);
+// }
+
+void add(HasPushBack auto &coll, const auto &val) { coll.push_back(val); }
+
+void add(auto &coll, const auto &val) { coll.insert(val); }
+
+int main() {
+  std::vector<int> coll1;
+  std::set<int> coll2;
+
+  static_assert(HasPushBack<std::vector<int>>);
+  static_assert(!HasPushBack<std::set<int>>);
+
+  add(coll1, 42);
+  add(coll2, 42);
+  return 0;
+}
